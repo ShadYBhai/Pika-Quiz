31c12b5f594066b6439ee091f0210d0556dc3ffb
Ashish <105300000+ShadYBhai@users.noreply.github.com>
2022-05-26 10:40:02 +0530

    Add files via upload

---
+++ pika.js
@@ -0,0 +1,17 @@
+const correctAnwsers = ['A','A','A','A','B','A','A','B','B'];
+const form = document.querySelector('.quiz-form');
+const h2 = document.querySelector('.ans')
+
+form.addEventListener ('submit',e => {
+    e.preventDefault()
+    let score = 0 ;
+    const userAns = [form.q1.value,form.q2.value,form.q3.value,form.q4.value, form.q5.value, form.q6.value, form.q7.value, form.q8.value ,form.q9.value]
+
+    userAns.forEach((ans,index) => {
+        if (ans === correctAnwsers[index] ){
+            score += 11.11;
+        }
+   })
+   h2.textContent = `YOUR CHILD'S HAPPINESS SCORE IS ${score}`
+    console.log(score)
+})
